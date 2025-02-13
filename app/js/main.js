ba63d150eef44658eb34e1016e8d6b80031cddbf
Daniel <dan.bisyukov@gmail.com>
2025-02-13 13:24:35 +0300

    I removed the scripts from the thanksPage page and improved the openCloseSidebar() function.

--- app/js/main.js
+++ app/js/main.js
@@ -7,25 +7,41 @@ const fixedHeaderWhenScrolling = () => {
     }
 }
 
-const openSidebar = () => {
+const openCloseSidebar = () => {
     const openBtn = document.querySelector('.header__burger');
     const closeBtn = document.querySelector('.sidebar__close');
-
     const sidebar = document.querySelector('.sidebar');
+
+    const toggleScroll = (isOpen) => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+    };
 
     openBtn.addEventListener('click', () => {
         sidebar.classList.add('sidebar--open');
+        toggleScroll(true);
     });
 
     closeBtn.addEventListener('click', () => {
         sidebar.classList.remove('sidebar--open');
+        toggleScroll(false);
     });
-}
+
+    document.addEventListener('click', (event) => {
+        if (!sidebar.contains(event.target) && !openBtn.contains(event.target)) {
+            sidebar.classList.remove('sidebar--open');
+            toggleScroll(false);
+        }
+    });
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', function () {
         fixedHeaderWhenScrolling();
     });
 
-    openSidebar();
+    openCloseSidebar();
 });
